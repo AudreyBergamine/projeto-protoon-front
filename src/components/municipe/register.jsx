bae5e9f6d525274afd5dce4d54023406e8010cbe
vagnero <vagnerimperador16@gmail.com>
2024-03-14 15:53:20 -0300

    configuração para atualizar os valores do forms de Endereco

--- src/components/municipe/register.jsx
+++ src/components/municipe/register.jsx
@@ -10,7 +10,7 @@ function RegisterForm() {
     email: "",
     senha: "",
     num_CPF: "",
-    data_nascimento: "2020-04-04",
+    data_nascimento: "",
     endereco: {
         tipo_endereco: "",
         num_cep: "",
@@ -28,10 +28,26 @@ function RegisterForm() {
 
   //Esta função tem o propósito de inserir valores nos dados acima, que estão vázios.
   const handleChange = (e) => {
+    //A lista abaixo contém o nome de todos os campos que há em endereço
+    const enderecoFields = ['tipo_endereco', 'num_cep', 'logradouro', 'nome_endereco', 'num_endereco', 'complemento', 'bairro', 'cidade', 'estado', 'pais'];
+
+    const { name, value } = e.target;
+    
+    if (enderecoFields.includes(name)) {//Caso em um formulário contenha algum nome da lista, então será alterado o valor do objeto endereco
+    setFormData({
+      ...formData,
+      endereco: {
+        ...formData.endereco,
+        [name]: value
+      }
+    });
+    
+  }else{//Caso não, será atualizado o campo municipe (todos os outros campos).
     setFormData({
       ...formData,
       [e.target.name]: e.target.value
     });
+  }
   };
 
   //A função abaixo lida com a conexão com o backend e a requisição de cadastrar um municipe.
@@ -105,61 +121,61 @@ function RegisterForm() {
     <div>
         <label>Tipo de endereço:</label>
         <input type="text" name="tipo_endereco" value={formData.tipo_endereco}
-          onChange={(handleChange)}
+          onChange={handleChange}
         />
       </div>
       <div>
         <label>Número do cep:</label>
         <input type="number" name="num_cep" value={formData.num_cep}
-          onChange={(handleChange)}
+          onChange={handleChange}
         />
       </div>
       <div>
         <label>Logradouro:</label>
         <input type="text" name="logradouro" value={formData.logradouro}
-          onChange={(handleChange)}
+          onChange={handleChange}
         />
       </div>
       <div>
         <label>Nome Endereço:</label>
         <input type="text" name="nome_endereco" value={formData.nome_endereco}
-          onChange={(handleChange)}
+          onChange={handleChange}
         />
       </div>
       <div>
         <label>Número do endereço:</label>
         <input type="number" name="num_endereco" value={formData.num_endereco}
-          onChange={(handleChange)}
+          onChange={handleChange}
         />
       </div>
       <div>
         <label>Complemento:</label>
         <input type="text" name="complemento" value={formData.complemento}
-          onChange={(handleChange)}
+          onChange={handleChange}
         />
       </div>
       <div>
         <label>Bairro:</label>
         <input type="text" name="bairro" value={formData.bairro}
-          onChange={(handleChange)}
+          onChange={handleChange}
         />
       </div>
       <div>
         <label>Cidade:</label>
         <input type="text" name="cidade" value={formData.cidade}
-          onChange={(handleChange)}
+          onChange={handleChange}
         />
       </div>
       <div>
         <label>Estado:</label>
         <input type="text" name="estado" value={formData.estado}
-          onChange={(handleChange)}
+          onChange={handleChange}
         />
       </div>
       <div>
         <label>País:</label>
         <input type="text" name="pais" value={formData.pais}
-          onChange={(handleChange)}
+          onChange={handleChange}
         />
       </div>
       
