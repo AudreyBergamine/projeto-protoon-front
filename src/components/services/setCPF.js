4b44ca8c603e8922e6f31b15e75eeaf11ee2f51e
Wesley Paulo Siqueira Soares <wesleyoares7@gmail.com>
2024-04-14 08:42:54 -0300

    cpf validation

--- src/components/services/setCPF.js
+++ src/components/services/setCPF.js
@@ -3,6 +3,34 @@ import React, { useState } from 'react';
 const SetCPF = ({ onCPFChange }) => {
     const [cpf, setCpf] = useState('');
 
+    const isCpfValido = (strCPF) => {
+        var Soma;
+        var Resto;
+        Soma = 0;
+
+        const cpf = strCPF.replace(/\D/g, ''); // Remove caracteres não numéricos
+
+        if (cpf.length !== 11) return false;
+
+        if (cpf === "00000000000") return false;
+
+        for (let i = 1; i <= 9; i++) 
+        Soma += parseInt(cpf.substring(i - 1, i)) * (11 - i);
+        Resto = (Soma * 10) % 11;
+        
+        if ((Resto === 10) || (Resto === 11)) Resto = 0;
+        if (Resto !== parseInt(cpf.substring(9, 10))) return false;
+        console.log('CPF ok: ', cpf)
+        
+        Soma = 0;
+        for (let i = 1; i <= 10; i++) Soma = Soma + parseInt(cpf.substring(i - 1, i)) * (12 - i);
+        Resto = (Soma * 10) % 11;
+        
+        if ((Resto === 10) || (Resto === 11)) Resto = 0;
+        if (Resto !== parseInt(cpf.substring(10, 11))) return false;
+        return true;
+    };
+
     const formatCpf = (event) => {
         let value = event.target.value;
         value = value.replace(/\D/g, '');
@@ -23,15 +51,19 @@ const SetCPF = ({ onCPFChange }) => {
     };
 
     return (
-        <input
-            type="text"
-            name="cpf"
-            placeholder="Ex.: 333.333.333-33"
-            value={cpf}
-            onChange={formatCpf}
-            required
-            minLength={14}
-        />
+        <>
+            <input
+                type="text"
+                name="cpf"
+                placeholder="Ex.: 333.333.333-33"
+                value={cpf}
+                onChange={formatCpf}
+                required
+                minLength={14}
+            />
+            {cpf.length === 14 && !isCpfValido(cpf) ? <span style={{ color: 'red' }}>CPF inválido</span> :
+            cpf.length === 14 && <span style={{ color: 'green' }}>CPF válido</span>}
+        </>
     );
 };
 
