cc879e09529b36785e3d712033f343004d3847fd
vagnero <vagnerimperador16@gmail.com>
2024-05-02 07:33:25 -0300

    vai

--- src/components/services/url.js
+++ src/components/services/url.js
@@ -1,5 +1,5 @@
-// const URL = 'https://projeto-protoon-back.onrender.com';
-const URL = 'http://localhost:8080';
+const URL = 'https://projeto-protoon-back.onrender.com';
+//const URL = 'http://localhost:8080';
 
 
 export default URL;
