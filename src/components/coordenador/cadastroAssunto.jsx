42785d51927312a1b3b6ca5ac7ed4143fc01a6aa
JLeandrodc <leandro.frot@hotmail.com>
2025-03-21 13:36:16 -0300

    cadastro de assunto funcionando

--- src/components/coordenador/cadastroAssunto.jsx
+++ src/components/coordenador/cadastroAssunto.jsx
@@ -1,19 +1,19 @@
 import React, { useState, useEffect } from "react";
 import axios from 'axios';
 import URL from '../services/url';
+import '../css/cadastroAssunto.css';
 
 const CadastroAssunto = () => {
     const [formData, setFormData] = useState({
-        problema: '',
-        preco: '',
+        assunto: '',
+        valor_protocolo: '',
         prioridade: '',
-        secretaria: '',
-        idSecretaria: null,
+        id_secretaria: 0, // Mantém o id_secretaria
     });
 
-    const [secretarias, setSecretarias] = useState([]); // Estado para armazenar a lista de secretarias
-    const [loading, setLoading] = useState(false); // Estado para carregamento
-    const [error, setError] = useState(null); // Estado para erros
+    const [secretarias, setSecretarias] = useState([]); // armazena a lista de secretarias
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState(null);
 
     const axiosInstance = axios.create({
         baseURL: URL,
@@ -43,107 +43,102 @@ const CadastroAssunto = () => {
             ...formData,
             [name]: value,
         });
-
-        // Atualiza o idSecretaria quando a secretaria é selecionada
-        if (name === 'secretaria') {
-            const selectedSecretaria = secretarias.find(secretaria => secretaria.nome_secretaria === value);
-            setFormData(prevState => ({
-                ...prevState,
-                idSecretaria: selectedSecretaria ? selectedSecretaria.id_secretaria : null,
-            }));
-        }
     };
 
     const handleSubmit = async (e) => {
         e.preventDefault();
         setLoading(true);
         try {
-            const response = await axiosInstance.post('/protoon/assunto', formData);
+            const response = await axiosInstance.post('/protoon/assuntos/registrar-assunto', formData);
             console.log('Assunto cadastrado com sucesso:', response.data);
-            // Limpar o formulário após o cadastro
+            // Limpa o formulário após o cadastro
             setFormData({
-                problema: '',
-                preco: '',
+                assunto: '',
+                valor_protocolo: '',
                 prioridade: '',
-                secretaria: '',
-                idSecretaria: null,
+                id_secretaria: 0,
             });
         } catch (error) {
             console.error('Erro ao cadastrar o assunto:', error);
             setError('Erro ao cadastrar o assunto.');
+            console.log(handleSubmit)
         } finally {
             setLoading(false);
         }
     };
 
     return (
-        <div>
-            <h1>Cadastro de Assunto</h1>
-            {error && <p style={{ color: 'red' }}>{error}</p>}
-            <form onSubmit={handleSubmit}>
-                <div>
-                    <label>
-                        Problema:
-                        <input
-                            type="text"
-                            name="problema"
-                            value={formData.problema}
-                            onChange={handleChange}
-                            required
-                        />
-                    </label>
-                </div>
-                <div>
-                    <label>
-                        Preço:
-                        <input
-                            type="number"
-                            name="preco"
-                            value={formData.preco}
-                            onChange={handleChange}
-                            required
-                        />
-                    </label>
-                </div>
-                <div>
-                    <label>
-                        Prioridade:
-                        <select
-                            name="prioridade"
-                            value={formData.prioridade}
-                            onChange={handleChange}
-                            required
-                        >
-                            <option value="">Selecione a prioridade</option>
-                            <option value="Baixa">Baixa</option>
-                            <option value="Média">Média</option>
-                            <option value="Alta">Alta</option>
-                        </select>
-                    </label>
-                </div>
-                <div>
-                    <label>
-                        Secretaria:
-                        <select
-                            style={{ fontSize: 20, padding: 10, borderRadius: 10, textAlign: "center" }}
-                            name="secretaria"
-                            value={formData.secretaria}
-                            onChange={handleChange}
-                            required
-                        >
-                            <option value="">Selecione uma secretaria</option>
-                            {secretarias.map(secretaria => (
-                                <option key={secretaria.id_secretaria} value={secretaria.nome_secretaria}>
-                                    {secretaria.nome_secretaria}
-                                </option>
-                            ))}
-                        </select>
-                    </label>
-                </div>
-                <button type="submit" disabled={loading}>
-                    {loading ? 'Cadastrando...' : 'Cadastrar'}
-                </button>
-            </form>
+        <div className="containerPrincipal">
+            <div className="containerSecundario">
+                <h1>Cadastro de Assunto</h1>
+                {error && <p style={{ color: 'red' }}>{error}</p>}
+                <form onSubmit={handleSubmit}>
+                    <div className="containerTerciario">
+                        <div>
+                            <label>
+                                Assunto:
+                                <input
+                                    type="text"
+                                    name="assunto"
+                                    value={formData.assunto}
+                                    onChange={handleChange}
+                                    required
+                                />
+                            </label>
+                        </div>
+                        <div>
+                            <label>
+                                Preço:
+                                <input
+                                    type="number"
+                                    name="valor_protocolo"
+                                    value={formData.valor_protocolo}
+                                    onChange={handleChange}
+                                    required
+                                />
+                            </label>
+                        </div>
+                        <div>
+                            <label>
+                                Prioridade:
+                                <select
+                                    name="prioridade"
+                                    value={formData.prioridade}
+                                    onChange={handleChange}
+                                    required
+                                >
+                                    <option value="">Selecione a prioridade</option>
+                                    <option value="BAIXA">Baixa</option>
+                                    <option value="MEDIA">Média</option>
+                                    <option value="ALTA">Alta</option>
+                                    <option value="URGENTE">Urgente</option>
+                                </select>
+                            </label>
+                        </div>
+                        <div>
+                            <label>
+                                Secretaria:
+                                <select
+                                    name="id_secretaria"
+                                    value={formData.id_secretaria}
+                                    onChange={handleChange}
+                                    required
+                                >
+                                    <option value={''}>Selecione uma secretaria</option>
+                                    {secretarias.map(secretaria => (
+                                        <option key={secretaria.id_secretaria} value={secretaria.id_secretaria}>
+                                            {secretaria.nome_secretaria}
+                                        </option>
+                                    ))}
+                                </select>
+                            </label>
+                        </div>
+                    </div>
+                    <button type="submit" className="btn-log" disabled={loading}>
+                        {loading ? 'Cadastrando...' : 'Cadastrar'}
+                    </button>
+                </form>
+            </div>
         </div>
     );
 };
